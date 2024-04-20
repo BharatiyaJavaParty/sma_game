5f89733791519b73d86508202fc8cf2aaadda671
BhargavNikumbh <72493359+BhargavNikumbh@users.noreply.github.com>
2024-04-20 14:08:39 +0100

    1. Applying changes to GameEngine.java

---
+++ src/main/java/bjp/constants/AppConstants.java
@@ -0,0 +1,15 @@
+package bjp.constants;
+
+public class AppConstants {
+    public final static int CO2_BUDGET = 500;
+
+    //CO2 Reductions
+    public final static int WALKING_CO2_REDUCTION = 5;
+    public final static int LUAS_CO2_REDUCTION = 20;
+    public final static int BUS_CO2_REDUCTION = 30;
+
+    //Time Increments
+    public final static int WALKING_TIME_INCREMENT = 10;
+    public final static int LUAS_TIME_INCREMENT = 5;
+    public final static int BUS_TIME_INCREMENT = 7;
+}
