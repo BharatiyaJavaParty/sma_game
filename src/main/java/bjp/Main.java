c7e4fb5c4103eb4fbf5750f1b6a6235f4e4c10b7
musaddique333 <mohammedmusaddiquemogal@gmail.com>
2024-04-25 12:07:24 +0100

    "Added cityMapLoaded flag, set onCloseRequest for primaryStage, and imported necessary classes for ScoreBoardController and GameEngine in Main.java"

--- src/main/java/bjp/Main.java
+++ src/main/java/bjp/Main.java
@@ -2,6 +2,8 @@ package bjp;
 
 import java.io.IOException;
 
+import bjp.controller.ScoreBoardController;
+import bjp.utility.GameEngine;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,18 +13,23 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     public static Scene scene;
-    //using this in PopupController
-    public static Stage mainStage;
+    public static boolean cityMapLoaded = false;
 
     @Override
     public void start(Stage primaryStage) throws IOException {
         try {
             // loads the fmxl file containing the first screen/ main screen of the game
             // user has 2 options on this screen
-            mainStage = primaryStage;
             primaryStage.setTitle("GEM HUNTER");
             primaryStage.setFullScreen(true);
             scene = new Scene(loadFXML("launch-view"), 1280, 720);
+
+            primaryStage.setOnCloseRequest(event -> {
+                if (cityMapLoaded) {
+                    ScoreBoardController.saveResults(GameEngine.gemCount);
+                    System.exit(0);
+                }
+            });
 
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -34,6 +41,9 @@ public class Main extends Application {
     public static void setRoot(String fxml) throws IOException {
         // FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxml +
         // ".fxml"));
+        if (fxml == "CityMap"){
+            cityMapLoaded = true;
+        }
         scene.setRoot(loadFXML(fxml));
     }
 
